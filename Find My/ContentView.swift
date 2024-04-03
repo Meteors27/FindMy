a753605417e68e01296c4d48ff9c310067db80c9
meteors27 <xia.yucheng@outlook.com>
2024-04-03 15:55:29 +0800

    Initial Commit

---
+++ Find My/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Find My
+//
+//  Created by william on 2024/4/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Find My/Find_MyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_MyApp.swift
+//  Find My
+//
+//  Created by william on 2024/4/3.
+//
+
+import SwiftUI
+
+@main
+struct Find_MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
